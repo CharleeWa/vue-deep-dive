0f103c479dbf62ca55aeec0558c29f6866ea7f54
CharleeWa <18888351756@163.com>
2023-02-08 23:06:14 +0800

    wip: 实现 `track`, 依赖收集(#1)

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -5,10 +5,28 @@ class ReactiveEffect {
   }
 
   run() {
+    activeEffect = this
     this._fn()
   }
 }
 
+const targetMap = new Map()
+export function track(target, key) {
+  let depsMap = targetMap.get(target)
+  if(!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  let dep = depsMap.get(key)
+  if(!dep) {
+    dep = new Set()
+  }
+
+  dep.add(activeEffect)
+}
+
+let activeEffect
 export function effect(fn){
   const _effect = new ReactiveEffect(fn)
   _effect.run()
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,8 +1,11 @@
+import { track } from "./effect"
+
 export function reactive(raw) {
   return new Proxy(raw, {
     get(target, key) {
 
       // TODO 依赖收集
+      track(target, key)
       return Reflect.get(target, key)
     },
 
