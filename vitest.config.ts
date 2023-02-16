d05fb65858cd9a5019d5dcde11914b9a0e72450b
CharleeWa <18888351756@163.com>
2023-02-17 00:48:57 +0800

    chore: 处理别名问题

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
   test: {
     globals: true
+  },
+  resolve: {
+    alias:[{
+      find: /@vue-deep-dive\/(\w*)/,
+      replacement: path.resolve(__dirname, 'packages') + '/$1/src'
+    }]
   }
 })
